da8b3e3a973b5ca2ee5dd6263f09209935b11ade
HuuTruong1 <huutruong1410@gmail.com>
2024-10-13 21:53:01 +0700

    Fixed Oauth

--- server/d-models/admin.model.ts
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from 'mongoose';
-import { IAdmin } from '../types/IAdmin';
-
-const AdminSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-});
-
-export const Admin = model<IAdmin>('Admin', AdminSchema);
--- server/d-models/jobSaved.model.ts
+++
@@ -1,9 +0,0 @@
-import { IJobSaved } from './../src/types/ICandidate.d';
-import { Schema, model } from 'mongoose';
-const JobSavedSchema = new Schema({
-  candidateId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  jobPostId: { type: Schema.Types.ObjectId, ref: 'JobPost', required: true },
-  savedAt: { type: Date, default: Date.now() }
-});
-
-export const JobSaved = model<IJobSaved>('JobSaved', JobSavedSchema);
